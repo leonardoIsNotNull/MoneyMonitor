535b7e6d15258e414036e8a3dfede8e00e5e0bfb
Leonardo Pereira <leonardocoutope@gmail.com>
2024-05-07 14:56:31 -0300

    chore: configurando back-end para lidar com requisições HTTP e comunicar com o front-end local

--- money-monitor-backend/src/main/java/com/leonardo/moneymonitorbackend/MoneyMonitorBackendApplication.java
+++ money-monitor-backend/src/main/java/com/leonardo/moneymonitorbackend/MoneyMonitorBackendApplication.java
@@ -2,12 +2,25 @@ package com.leonardo.moneymonitorbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@EnableWebMvc
+@EnableJpaRepositories
 @SpringBootApplication
-public class MoneyMonitorBackendApplication {
+public class MoneyMonitorBackendApplication implements WebMvcConfigurer{
 
     public static void main(String[] args) {
         SpringApplication.run(MoneyMonitorBackendApplication.class, args);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH");
     }
 
 }
