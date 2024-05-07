d91686e00077b272a522f54fdbb76390e4c90973
Leonardo Pereira <leonardocoutope@gmail.com>
2024-05-07 15:13:43 -0300

    chore: criação do arquivo de constantes para requisições para o servidor

---
+++ money-monitor-frontend/src/app/AppConstansts.ts
@@ -0,0 +1,8 @@
+export class AppConstansts {
+
+  static readonly URL_BASE_SERVIDOR: string = 'http://localhost:8080/';
+
+  public static getBaseServidor(): string {
+    return this.URL_BASE_SERVIDOR;
+  }
+}
